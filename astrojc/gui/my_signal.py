b0f5f91a4b73c62ee5743bc2327fcab5d9e5ff06
Julio Campagnolo <juliocampagnolo@gmail.com>
2017-02-10 19:57:35 -0200

    signal emulation support

---
+++ astrojc/gui/my_signal.py
@@ -0,0 +1,31 @@
+'''
+Simple class to emulate signals in python.
+'''
+
+class MySignal(object):
+    '''
+    This class implements a simple way to handle signals in Python, without use
+    qt, but with similar (simpler) behavior. No slot is supported.
+    '''
+    def __init__(self):
+        self._emit_function = None
+
+    def connect(self, function):
+        '''
+        Connect the function that will be executed when the signal is emitted.
+        '''
+        self._emit_function = function
+
+    def disconnect(self):
+        '''
+        Disconnect the function exectution when the signal is emitted.
+        '''
+        self._emit_function = None
+
+    def emit(self, *args, **kwargs):
+        '''
+        Execute the connected functions, passing to them the args and kwargs.
+        '''
+        if self._emit_function is not None:
+            return self._emit_function(*args, **kwargs)
+        return None
