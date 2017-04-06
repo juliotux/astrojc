edb464b4cd26c26df293f428502b80b3419e391d
Julio Campagnolo <juliocampagnolo@gmail.com>
2017-04-06 13:39:49 -0300

    added kwargs support for plot_lightcurve

--- astrojc/photometry/photplot.py
+++ astrojc/photometry/photplot.py
@@ -12,8 +12,9 @@ def plot_twiny(ax, begin, end, step):
     axyear.set_xlabel('Year')
 
 def plot_lightcurve(ax, jd, flux, jd_error=None, flux_error=None, offset=0, fmt='ko',
-                    label=None):
+                    label=None, pltkwargs={}):
     if flux_error is not None or jd_error is not None:
-        ax.errorbar(jd, flux + offset, xerr=jd_error, yerr=flux_error, fmt=fmt, label=label)
+        ax.errorbar(jd, flux + offset, xerr=jd_error, yerr=flux_error, fmt=fmt, label=label
+                    **pltkwargs)
     else:
-        ax.plot(jd, flux + offset, fmt, label=label)
+        ax.plot(jd, flux + offset, fmt, label=label, **pltkwargs)
