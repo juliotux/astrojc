7d925d8b3e38cf5b2b2cc6e11b393370b8fa4994
Julio Campagnolo <juliocampagnolo@gmail.com>
2017-04-12 15:25:01 -0300

    pep8 fixing

--- astrojc/fitting/modelreader.py
+++ astrojc/fitting/modelreader.py
@@ -1,9 +1,9 @@
 from astropy.io import fits
-from astropy.table import Table
 from os import path
 import numpy as np
 
 from ..logging import log
+
 
 def _float_equal(f1, f2, precision):
     if abs(float(f1) - float(f2)) < precision:
@@ -11,12 +11,17 @@ def _float_equal(f1, f2, precision):
     else:
         return False
 
+
 def read_castelli_model(folder, metallicity, teff, logg, catalog='catalog.fits'):
+    """Read Castelli models from a folder, for a given metallicity, Teff, logg
+    and a Castelli fits catalog.
+    """
     c = fits.open(path.join(folder, catalog))
     fname = None
     for i in c[1].data:
-        t,m,g = [np.float(k) for k in i[0].split(',')]
-        if _float_equal(t, teff, 1.0) and _float_equal(m, metallicity, 0.1) and _float_equal(g, logg, 0.1):
+        t, m, g = [np.float(k) for k in i[0].split(',')]
+        if _float_equal(t, teff, 1.0) and \
+           _float_equal(m, metallicity, 0.1) and _float_equal(g, logg, 0.1):
             fname = i[1]
             log.info('Loading Castelli model %s' % fname)
             pass
@@ -25,5 +30,5 @@ def read_castelli_model(folder, metallicity, teff, logg, catalog='catalog.fits')
         raise ValueError("File with these parameters doesn't exists")
 
     f = fits.open(path.join(folder, fname.split('[')[0]))[1].data
-    col = fname.split('[')[1].replace(']','')
+    col = fname.split('[')[1].replace(']', '')
     return f['WAVELENGTH'], f[col]
