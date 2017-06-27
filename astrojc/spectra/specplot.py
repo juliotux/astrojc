66775bcddfe95f38a342a9ccb0b302923cfc2666
Julio Campagnolo <juliocampagnolo@gmail.com>
2017-06-27 18:29:13 -0300

    kwargs for specplot helpers

--- astrojc/spectra/specplot.py
+++ astrojc/spectra/specplot.py
@@ -25,17 +25,20 @@ def _join_strings(arr1, arr2):
             outarray.append(str(arr1[i]) + ' m' + str(arr2[i]))
     return outarray
 
-def plot_spec(ax, spec, offset=0, fmt='k-', label=None, **kwargs):
+def plot_spec(ax, spec, offset=0, fmt='k-', label=None, pltkwargs={}, **kwargs):
     '''
     Plots just one spectrum with a offset in and axis.
     '''
-    ax.plot(spec.wave, spec.flux + offset, fmt, label=label, **kwargs)
+    ax.plot(spec.wave, spec.flux + offset, fmt, label=label, **pltkwargs,
+            **kwargs)
 
-def plot_spec_vel(ax, spec, wcenter, fmt='k-', offset=0, label=None, **kwargs):
+def plot_spec_vel(ax, spec, wcenter, fmt='k-', offset=0, label=None,
+                  pltkwargs={}, **kwargs):
     '''
     Plots just one spectrum with a offset in and axis.
     '''
-    ax.plot(to_vel(wcenter, spec.wave), spec.flux + offset, fmt, label=label, **kwargs)
+    ax.plot(to_vel(wcenter, spec.wave), spec.flux + offset, fmt, label=label,
+            **pltkwargs, **kwargs)
 
 def ticks_ids(ax, wave, text):
     axn = ax.twiny()
@@ -44,10 +47,15 @@ def ticks_ids(ax, wave, text):
     axn.set_xlim(ax.get_xlim())
     axn.grid()
 
-def plot_line_id(ax, wave, y, text, box_size, spec=None, flux=None, wavetext=None):
+def plot_line_id(ax, wave, y, text, box_size, spec=None, flux=None,
+                 wavetext=None, pltkwargs={}):
     if wavetext is None:
         wavetext = wave #colocar iterador para definir novo wavetext
     if spec is not None:
-        ax.annotate(text, xy=(wave, np.interp(spec.wave, spec.flux, wave)), xytext=(wavetext, y), arrowprops=dict(arrowstyle='-'), ha='center', va='bottom')
+        ax.annotate(text, xy=(wave, np.interp(spec.wave, spec.flux, wave)),
+                    xytext=(wavetext, y), arrowprops=dict(arrowstyle='-'),
+                    ha='center', va='bottom', **pltkwargs)
     elif flux is not None:
-        ax.annotate(text, xy=(wave, flux), xytext=(wavetext, y), arrowprops=dict(arrowstyle='-'), ha='center', va='bottom')
+        ax.annotate(text, xy=(wave, flux), xytext=(wavetext, y),
+                    arrowprops=dict(arrowstyle='-'), ha='center', va='bottom',
+                    **pltkwargs)
