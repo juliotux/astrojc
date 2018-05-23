cf625b33a01a7e0504a451605c1c0ab2bc5884d0
Julio Campagnolo <juliocampagnolo@gmail.com>
2018-05-23 16:24:56 -0300

    Pipeline transported to astropop

--- astrojc/pipeline/__init__.py
+++
@@ -1,7 +0,0 @@
-from .inputs import *
-from .outputs import *
-from .nodes import *
-from .connections import *
-from .context import *
-from .pipeline import *
-from .utils import *
--- astrojc/reducing/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import astromatic_wrapper as astromatic
